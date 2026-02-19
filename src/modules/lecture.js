f6065825de8e20ea139d5a1b47be52b87f16786e
Galindo Adrian <galindo.adrian308@gmail.com>
2026-02-19 16:31:44 +0100

    preparatives pour saisie ficher coordonÃÃnees

--- src/modules/lecture.js
+++ src/modules/lecture.js
@@ -1,11 +1,6 @@
 import {setPointIntoCollection} from "./parsing.js";
 import {validationSaisiRegex} from "../utils/validation.js";
 
-let input_coordonnees_manuel = document.getElementById("coordonnees_manuel");
-let message_error_manuel = document.getElementById("message_error_saisie_manuel");
-let affichage_coordonnees_manuel = document.getElementById("affichage_coordonnees_manuel");
-let button_submit_coordonnees_manuel = document.getElementById("btn_submit_coordonnees_manuel");
-
 // Fonction pour créer un élément li pour afficher un point
 function createBaliseLiByPoint(x, y) {
     let li = document.createElement("li");
@@ -14,70 +9,85 @@ function createBaliseLiByPoint(x, y) {
 }
 
 // Fonction pour réinitialiser le champ et les messages
-function resetInput() {
+function resetInputSaisieManuel() {
     input_coordonnees_manuel.value = "";
     button_submit_coordonnees_manuel.disabled = true;
     message_error_manuel.textContent = "";
 }
 
-// Écouteur d'événements pour l'entrée des coordonnées manuelles
-input_coordonnees_manuel.addEventListener("input", function(event) {
-    let value = input_coordonnees_manuel.value;
+function lectureCoordonneesManuel() {
 
-    // Si l'entrée est vide, désactiver le bouton et réinitialiser les messages
-    if(value.trim() === "") {
-        button_submit_coordonnees_manuel.disabled = true;
-        message_error_manuel.textContent = "";
-        return;
-    }
+    let input_coordonnees_manuel = document.getElementById("coordonnees_manuel");
+    let message_error_manuel = document.getElementById("message_error_saisie_manuel");
+    let affichage_coordonnees_manuel = document.getElementById("affichage_coordonnees_manuel");
+    let button_submit_coordonnees_manuel = document.getElementById("btn_submit_coordonnees_manuel");
 
-    let match = validationSaisiRegex(value);
+    // Écouteur d'événements pour l'entrée des coordonnées manuelles
+    input_coordonnees_manuel.addEventListener("input", function(event) {
+        let value = input_coordonnees_manuel.value;
 
-    if (match) {
-        // Activer le bouton de soumission si les coordonnées sont valides
-        button_submit_coordonnees_manuel.disabled = false;
-
-        // Réinitialiser les messages d'erreur
-        message_error_manuel.textContent = "";
-    } else {
-        // Désactiver le bouton et réinitialiser l'affichage en cas d'erreur
-        button_submit_coordonnees_manuel.disabled = true;
-        message_error_manuel.textContent = "Format de coordonnées invalide. Veuillez entrer au format 'X, Y'.";
-    }
-});
-
-// Écouteur pour le bouton de soumission des coordonnées
-button_submit_coordonnees_manuel.addEventListener("click", function() {
-    // On fait a nouveau une validation pour s'assurer que les coordonnées sont correctes avant de les afficher (possible manipulation de l'utilisateur)
-    let value = input_coordonnees_manuel.value;
-
-    let match = validationSaisiRegex(value);
-
-    if (match) {
-        let pointX = parseFloat(match[1]);
-        let pointY = parseFloat(match[3]);
-
-        try{
-            // Ajouter le point à la collection de points
-            setPointIntoCollection(pointX, pointY);
-
-            // Créer un élément <li> pour afficher le point
-            const li = createBaliseLiByPoint(pointX, pointY);
-            affichage_coordonnees_manuel.appendChild(li);
-
-            // Réinitialiser le champ et désactiver le bouton
-            resetInput();
-        }
-        catch (error) {
-            // Réinitialiser le champ et désactiver le bouton
-            resetInput();
-
-            // Afficher une erreur si l'ajout du point échoue (par exemple, si les coordonnées sont hors limites)
-            message_error_manuel.textContent = 'Erreur : ' + error.message;
+        // Si l'entrée est vide, désactiver le bouton et réinitialiser les messages
+        if(value.trim() === "") {
+            button_submit_coordonnees_manuel.disabled = true;
+            message_error_manuel.textContent = "";
+            return;
         }
 
-    } else {
-        // Afficher une erreur si le format est incorrect (bien que ce cas soit déjà géré en amont)
-        message_error_manuel.textContent = "Erreur : Coordonnées invalides.";
-    }
-});
+        let match = validationSaisiRegex(value);
+
+        if (match) {
+            // Activer le bouton de soumission si les coordonnées sont valides
+            button_submit_coordonnees_manuel.disabled = false;
+
+            // Réinitialiser les messages d'erreur
+            message_error_manuel.textContent = "";
+        } else {
+            // Désactiver le bouton et réinitialiser l'affichage en cas d'erreur
+            button_submit_coordonnees_manuel.disabled = true;
+            message_error_manuel.textContent = "Format de coordonnées invalide. Veuillez entrer au format 'X, Y'.";
+        }
+    });
+
+    // Écouteur pour le bouton de soumission des coordonnées
+    button_submit_coordonnees_manuel.addEventListener("click", function() {
+        // On fait a nouveau une validation pour s'assurer que les coordonnées sont correctes avant de les afficher (possible manipulation de l'utilisateur)
+        let value = input_coordonnees_manuel.value;
+
+        let match = validationSaisiRegex(value);
+
+        if (match) {
+            let pointX = parseFloat(match[1]);
+            let pointY = parseFloat(match[3]);
+
+            try{
+                // Ajouter le point à la collection de points
+                setPointIntoCollection(pointX, pointY);
+
+                // Créer un élément <li> pour afficher le point
+                const li = createBaliseLiByPoint(pointX, pointY);
+                affichage_coordonnees_manuel.appendChild(li);
+
+                // Réinitialiser le champ et désactiver le bouton
+                resetInputSaisieManuel();
+            }
+            catch (error) {
+                // Réinitialiser le champ et désactiver le bouton
+                resetInputSaisieManuel();
+
+                // Afficher une erreur si l'ajout du point échoue (par exemple, si les coordonnées sont hors limites)
+                message_error_manuel.textContent = 'Erreur : ' + error.message;
+            }
+
+        } else {
+            // Afficher une erreur si le format est incorrect (bien que ce cas soit déjà géré en amont)
+            message_error_manuel.textContent = "Erreur : Coordonnées invalides.";
+        }
+    });
+
+}
+
+function lectureCoordonneesFichier() {
+
+}
+
+lectureCoordonneesManuel()
